410fa6f80cbfdfda283d70bb0dac05b60520eb1c
Alexander Samsonov <102922048+namiron@users.noreply.github.com>
2024-08-14 14:16:20 +0300

    Update Result.tsx

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,31 +1,28 @@
-import React from 'react'
-import quiz from './../modules/questions.module.scss'
-import { RESULT, } from '../command/constant'
-import { IResultProps } from '../types/PropsTypes'
-import { questions } from '../command/constant'
-import img from '../image/confity.webp'
+import React from "react";
+import quiz from "./../modules/questions.module.scss";
+import { RESULT } from "../command/constant";
 
+import { questions } from "../command/constant";
+import img from "../image/confity.webp";
 
-
-const Result: React.FC<IResultProps> = ({ correct }) => {
-    //-------------------------------
+const Result = ({ correct }) => {
+  //-------------------------------
   const TITLE_RESULT = `Congratulations! You answered ${correct} out of ${questions.length} questions correctly.`;
 
-    //-------------------------------
+  //-------------------------------
 
+  return (
+    <div className={quiz.result}>
+      <div className={quiz.imageBox}>
+        <img src={img} alt="" />
+      </div>
+      <h2 className={quiz.titleResult}>{TITLE_RESULT}</h2>
+      <a href="/">
+        <button className={quiz.restart}>{RESULT}</button>
+      </a>
+    </div>
+  );
+};
 
-    return (
-        <div className={quiz.result}>
-            <div className={quiz.imageBox}>
-                <img src={img} alt="" />
-            </div>
-            <h2 className={quiz.titleResult}>{TITLE_RESULT}</h2>
-            <a href="/">
-                <button className={quiz.restart}>{RESULT}</button>
-            </a>
+export default Result;
 
-        </div>
-    )
-}
-
-export default Result
